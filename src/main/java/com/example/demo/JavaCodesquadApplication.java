f058f8057913c402181001b4f8658f1477bdc5e6
areumddle <areumddle@218.38.137.28>
2017-07-31 16:28:20 +0900

    initial commit

---
+++ src/main/java/com/example/demo/JavaCodesquadApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodesquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodesquadApplication.class, args);
+	}
+}
