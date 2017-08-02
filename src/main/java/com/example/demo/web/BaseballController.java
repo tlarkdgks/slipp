7bb3caea2e17b14cd6579bb94cedfd17180dbed4
areumddle <areumddle@218.38.137.28>
2017-08-02 17:12:01 +0900

    user list complete      profile unclear

--- src/main/java/com/example/demo/web/BaseballController.java
+++ src/main/java/com/example/demo/web/BaseballController.java
@@ -6,13 +6,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BaseballController {
-	ArrayList<Integer> computerBalls;
-	
 	@GetMapping("/baseball")
 	public ModelAndView input(String inputValue) {
-		if (computerBalls == null) {
-			computerBalls = Baseball.generateComputerBalls();
-		}
 		System.out.print("input value :" + inputValue);
 		ModelAndView mav = new ModelAndView("baseball/result");
 		mav.addObject("baseball", inputValue);
